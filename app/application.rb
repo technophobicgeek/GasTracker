a2ef662bbb8d159838fef7747159a49c7cb61c57
Raj Bandyopadhyay <plusbzz@gmail.com>
2011-05-10 18:09:47 -0400

    removed extraneous buttons

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,17 @@ require 'rho/rhoapplication'
 
 class AppApplication < Rho::RhoApplication
   def initialize
-    # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
-    # Super must be called *after* settings @tabs!
+
     @tabs = nil
-    #To remove default toolbar uncomment next line:
-    #@@toolbar = nil
+    @@tabbar = nil
+    
+    @@toolbar = [
+      {:action => :back},
+      {:action => :separator},
+      {:action => :separator},
+      {:action => :home}
+    ]
+    
     super
     $rholog = RhoLog.new
 
